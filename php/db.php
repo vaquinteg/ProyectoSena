4bd7a43c8ad0620321bbcbfa311f753ff0d0c1d5
vaquinteg <visitador352@gmail.com>
2024-07-25 23:30:10 -0500

    modificación ventana paciente

--- php/db.php
+++ php/db.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";  // Cambia esto si tienes un usuario diferente
 $password = "";  // Cambia esto si tienes una contraseña diferente
-$dbname = "pruebaphp";
+$dbname = "proyectosena";
 
 // Crear conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
