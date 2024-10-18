81b298d1936f1d98e3cf35f02dfa57fae4335878
vaquinteg <visitador352@gmail.com>
2024-10-17 19:40:45 -0500

    se hizo pacientes

--- crear_paciente.php
+++ crear_paciente.php
@@ -18,60 +18,46 @@
           <h1>Registrar paciente nuevo</h1>
         </div>
       </div>
-      
-      
-      
+                
           <div class="container-fluid row mt-5" >
             <div class="col-lg-6">
-              <form action="php/registrar_paciente.php" method="post">
-              <div class="row">
-                <div class="col-lg-4"> <label class="label" for="nombre_apellido">Nombres y apellidos</label> 
-                </div>
-                <div class="col-auto">          
-                <input class="campo_texto_nom" type="text" id="nombre_apellido" name="nombre_apellido" class="form-control" >
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-lg-4"> <label for="tipo_documento">Tipo de documento</label> </div>
-                <div class="col-auto">
-                    <select name="tipo_documento" class="classic" aria-label="tipo_documento">
-                        <option value=""> Seleccione un tipo de documento</option>
-                        
-                        <option value="CC">CC</option>
-                        <option value="CE">CE</option>
-                        <option value="TI">TI</option>
-                        <option value="NUIP">NUIP</option>
-                        <option value="PASAPORTE">PASAPORTE</option>
-                    </select>
-                </div>
-              </div>
+              <form id="formPaciente" action="php/registrar_paciente.php" method="post">
+              
+              <!--identificación -->
               <div class="row mt-3">
                 <div class="col-lg-4">     
-                <label for="numero_documento">Número de documento</label> 
+                <label for="identificacion">Número de documento</label> 
                 </div>
                 <div class="col-auto pt-2">          
-               <input class="CampoTexto" type="number" name="numero_documento" class="form-control" >
+               <input class="CampoTexto" type="number" name="identificacion"  id="identificacion" class="form-control" onblur="obtenerPaciente()" >
                 </div>
               </div>
-    
+              
+              <div class="row mt-3"> <!-- columna nombre-->
+                <div class="col-lg-4"> <label class="label" for="nombre">Nombres y apellidos</label> 
+                </div>
+                <div class="col-auto">          
+                <input readonly class="campo_texto_nom" type="text" id="nombre" name="nombre" class="form-control" >
+                </div>
+              </div>
+                  
               <div class="row">
                 <div class="col-lg-4 pt-3"> <label for="edad">Edad</label></div>
                 <div class="col-auto pt-2">
                 <input class="CampoTexto" type="number" name="edad" class="form-control" >
                 </div>
               </div>
-              <div class="row mt-3">
+              <div class="row mt-2">
                 <div class="col-lg-4"> <label for="RH">RH</label> </div>
                 <div class="col-auto">
                     <select name="rh" class="classic" aria-label="rh">
                         <option selected> Seleccione un RH</option>
                         <option value="+">+</option>
                         <option value="-">-</option>
-                
                     </select>
                 </div>
               </div>
-              <div class="row mt-3">
+              <div class="row mt-4">
                 <div class="col-lg-4"> <label for="grupo_sanguineo">Grupo sanguineo</label> </div>
                 <div class="col-auto">
                     <select name="grupo_sanguineo" class="classic" aria-label="grupo_sanguineo">
@@ -83,20 +69,20 @@
                     </select>
                 </div>
               </div>
-              <div class="row mt-3">
+              <div class="row mt-4">
                 <div class="col-lg-4">     
                 <label for="telefono">Número de teléfono</label> 
                 </div>
                 <div class="col-auto">          
-               <input class="CampoTexto" type="number" name="telefono" class="form-control" >
+               <input readonly class="CampoTexto" type="number" name="telefono" id="telefono" class="form-control" >
                 </div>
               </div>
-              <div class="row mt-3">
+              <div class="row mt-2">
                 <div class="col-lg-4">     
                 <label for="direccion">Dirección</label> 
                 </div>
                 <div class="col-auto">          
-               <input class="CampoTexto" type="text" name="direccion" class="form-control" >
+               <input  readonly class="CampoTexto" type="text" name="direccion" id="direccion" class="form-control" >
                 </div>
               </div>
               <div class=" row justify-content-center">
@@ -113,7 +99,8 @@
              
           </div> 
            
-            
+<script src="js/completar_paciente.js"></script> </script>
+<script src="js/resp_validacion_paciente.js"></script>
 </body>
 
 </html>
---
+++ js/resp_validacion_paciente.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var formulario = document.getElementById("formPaciente");
+
+    formulario.addEventListener("submit", function(event) {
+        event.preventDefault();  // Prevenir el envío normal del formulario
+
+        // Crear la solicitud XMLHttpRequest
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "http://localhost/ProyectoSena/ProyectoSena/php/registrar_paciente.php", true);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+        // Capturar los valores del formulario
+       // var identificacion = encodeURIComponent(document.getElementsByName("identificacion")[0].value);
+        var edad = encodeURIComponent(document.getElementsByName("edad")[0].value);
+        var rh = encodeURIComponent(document.getElementsByName("rh")[0].value);
+        var grupo_sanguineo = encodeURIComponent(document.getElementsByName("grupo_sanguineo")[0].value);
+       
+
+        // Preparar los datos para enviar
+        var data = "edad=" + edad + "&rh=" + rh + "&grupo_sanguineo=" + grupo_sanguineo;// +"&identificacion=" + identificacion;
+        xhr.send(data);
+
+        // Procesar la respuesta del servidor
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                try {
+                    var respuesta = JSON.parse(xhr.responseText);
+                    if (respuesta.estado === "exito") {
+                        alert("Éxito: " + respuesta.mensaje);
+                    } else if (respuesta.estado === "error") {
+                        alert("Error: " + respuesta.mensaje);
+                    }
+                } catch (e) {
+                    alert("Error al enviar información, revise la información ingresada.");
+                }
+            } else {
+                alert("Error: No se pudo procesar la solicitud.");
+            }
+        };
+
+        // Manejar errores de red o conexión
+        xhr.onerror = function() {
+            alert("Error: Problema de conexión con el servidor.");
+        };
+    });
+});
